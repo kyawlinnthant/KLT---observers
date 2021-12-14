d93a8e2d785c63a0e68a354e805d5104c89d2a83
unknown <kyawlinnthantkyawlinnthant@gmail.com>
2021-12-14 14:49:04 +0630

    completed with comments

--- app/src/main/java/com/example/klt_flow_stateflow_sharedflow/EmitViewModel.kt
+++ app/src/main/java/com/example/klt_flow_stateflow_sharedflow/EmitViewModel.kt
@@ -11,51 +11,67 @@ class EmitViewModel : ViewModel() {
 
     /** < LiveData > can be or cannot be initiate INITIAL VALUE
      *  If we declare initial value to liveData, the observer will know it when it observe our liveData.
-     *  It is very cold observer means it can only be emit data when something start call it (means observe it)
+     *  It is very cold observer means it can only be emitted data when something starts calling it (means observe it)
      *  It is only life cycle aware data, observed for view states in Main thread.
      */
-//    private val _liveData : MutableLiveData<String> = MutableLiveData("Initial Value LiveData")
-    private val _liveData : MutableLiveData<String> = MutableLiveData()
-    val liveData : LiveData<String> get() = _liveData
+    private val _liveData: MutableLiveData<String> = MutableLiveData("Initial Value LiveData")
+
+    /**
+     * If we start assign liveData with no default value, the caller from Activity can get no value and change nothing.
+     * When button is clicked, it starts call the fetchLiveData() and change the liveData
+     * Then textView in Activity just know the changes by observe.
+     * Use LiveData when you want to operate something like: you want to show user default value and show the user when
+     * user make something changes and show that changes ever!!!
+     */
+//    private val _liveData : MutableLiveData<String> = MutableLiveData()
+    val liveData: LiveData<String> get() = _liveData
+
 
     /** < Flow > can be or cannot be initiate INITIAL VALUE
      *  It is just a coroutines. But coroutines can only do with once for each data.
      *  Flow can do as coroutines with multiple changes of data.
+     *  But it is the continuous flow as its name. When it is once called, it starts its work FLOW.
+     *  Normal FLOW does not contain STATE.
      */
-//    private val _flowData: Flow<String> = flowOf("Initial Value")
-    private val _flowData: Flow<String> = flowOf()
+    private var _flowData: Flow<String> = flowOf("Initial Value Flow")
+//    private var _flowData: Flow<String> = flowOf()
     val flowData get() = _flowData
+
 
     /** < StateFlow > can only be initiated with INITIAL VALUE
      *  Like LiveData everything is the same except this StateFlow is kind of Flow.
      *  LiveData can or cannot initiate INITIAL VALUE, but StateFlow can only initiate INITIAL VALUE
-     *  LiveData is absolutely cold observer, it can only emit data when the caller call it,(in our case, click the button and call fetchLiveData())
+     *  LiveData is absolutely cold observer, it can only emit data when the caller start listen(observe) it.
      *  but after call, it can make the connection about data changes with the caller until the caller finish connection.
-     *  StateFlow is  hot observer means it can emit data whether or not it is called. (in our case, don't even need fetchStateFlow(), the caller
-     *  can get the stateFlow data just only use collect). So this is HOT observer.
+     *  StateFlow is  HOT observer means it can emit data whether or not it is called.
+     *  StateFlow also save the same data and not emit again the same data.
      */
     private val _stateFlowData = MutableStateFlow("Initial Value StateFlow")
     val stateFlowData get() = _stateFlowData.asStateFlow()
 
+
     /** < SharedFlow > cannot be initiate INITIAL VALUE
-     *  It is cold observer means it can emit data when it is called.
+     *  It is HOT observer means it can emit data whether it is called or not.
      *  It only emit data is changed however the caller calls it.
      *  Means that the last data it emitted is not changed when caller call it,
      *  it will not emit that the same data.
+     *  Use sharedFlow for one time event.
      */
-
     private val _sharedFlowData = MutableSharedFlow<String>()
     val sharedFlowData get() = _sharedFlowData.asSharedFlow()
 
-    fun fetchLiveData(){
+    fun fetchLiveData() {
         viewModelScope.launch {
             _liveData.postValue("This is live data")
         }
     }
 
-    fun fetchFlow() {
-        viewModelScope.launch {
-
+    fun fetchFlowData() {
+        _flowData = flow {
+            repeat(5) {
+                emit("$it")
+                kotlinx.coroutines.delay(1000L)
+            }
         }
     }
 
@@ -64,9 +80,10 @@ class EmitViewModel : ViewModel() {
             _stateFlowData.value = "This is stateFlow"
         }
     }
+
     fun fetchSharedFlow() {
         viewModelScope.launch {
-
+            _sharedFlowData.emit("This is sharedFlow")
         }
     }
 }
--- app/src/main/java/com/example/klt_flow_stateflow_sharedflow/ObserveActivity.kt
+++ app/src/main/java/com/example/klt_flow_stateflow_sharedflow/ObserveActivity.kt
@@ -1,42 +1,43 @@
 package com.example.klt_flow_stateflow_sharedflow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
-import org.w3c.dom.Text
+import kotlinx.coroutines.launch
 
 class ObserveActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private val tvLiveData : TextView by lazy {
+    private val tvLiveData: TextView by lazy {
         findViewById(R.id.tv_live_data)
     }
-    private val tvFlow : TextView by lazy {
+    private val tvFlow: TextView by lazy {
         findViewById(R.id.tv_flow)
     }
-    private val tvStateFlow : TextView by lazy {
+    private val tvStateFlow: TextView by lazy {
         findViewById(R.id.tv_state_flow)
     }
-    private val tvSharedFlow : TextView by lazy {
+    private val tvSharedFlow: TextView by lazy {
         findViewById(R.id.tv_shared_flow)
     }
-    private val btnLiveData : Button by lazy {
+    private val btnLiveData: Button by lazy {
         findViewById(R.id.btn_live_data)
     }
-    private val btnFlow : Button by lazy {
+    private val btnFlow: Button by lazy {
         findViewById(R.id.btn_flow)
     }
-    private val btnStateFlow : Button by lazy {
+    private val btnStateFlow: Button by lazy {
         findViewById(R.id.btn_state_flow)
     }
-    private val btnSharedFlow : Button by lazy {
+    private val btnSharedFlow: Button by lazy {
         findViewById(R.id.btn_shared_flow)
     }
-    private val viewModel : EmitViewModel by viewModels()
+    private val viewModel: EmitViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,20 +51,43 @@ class ObserveActivity : AppCompatActivity(R.layout.activity_main) {
         }
 
         /**
+         * Just watch how the flow is done.
+         * When you listen flowData with lifecycleScope.launch, it can get as fast as soon
+         * But it can emit multiple values sequentially, but ONLY A SINGLE VALUE.
+         * Therefore in our case we can't get the new data of FLOW when we click the button
+         * We can only get by rotate ( change lifecycle ) to get the new FLOW data.
+         */
+        btnFlow.setOnClickListener {
+            viewModel.fetchFlowData()
+        }
+
+        /**
          * Before click this button, just see our tvStateFlow.text
          * It is the initial value from ViewModel.
+         * And JUST WATCH THE TOAST MESSAGE, hot observer can listen fast
+         * So STATEFLOW value can reach first.
          * Now you can know what is the HOT OBSERVER?
          */
         btnStateFlow.setOnClickListener {
             viewModel.fetchStateFlow()
         }
+        btnSharedFlow.setOnClickListener {
+            viewModel.fetchSharedFlow()
+        }
     }
 
     private fun observe() {
 
-        viewModel.liveData.observe(this){
+        viewModel.liveData.observe(this) {
             tvLiveData.text = it
             showToast(it)
+        }
+
+        lifecycleScope.launch {
+            viewModel.flowData.collect {
+                tvFlow.text = it
+                showToast(it)
+            }
         }
 
         /** Just remember, to use StateFlow we have to observe it in our lifecycleScope.
@@ -78,9 +102,17 @@ class ObserveActivity : AppCompatActivity(R.layout.activity_main) {
                 showToast(it)
             }
         }
+
+        lifecycleScope.launch {
+            viewModel.sharedFlowData.collect {
+                tvSharedFlow.text = it
+                showToast(it)
+            }
+
+        }
     }
 
     private fun showToast(text: String) {
-        Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
